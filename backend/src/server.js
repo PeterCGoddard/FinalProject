ed407942b05abd337471e9867d739487d2f48892
PeterCGoddard <petercgoddard@gmail.com>
2025-12-04 21:41:48 -0600

    Change default server port from 27017 to 5001

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 27017;
+const PORT = process.env.PORT || 5001;
 const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI)
     .then(() =>
